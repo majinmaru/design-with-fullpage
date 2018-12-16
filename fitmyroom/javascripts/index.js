e97a1dd8360d091e545c1c728c9887b984863a6c
majinmaru <gotch11@naver.com>
2018-12-16 16:41:18 +0900

    18.12.16
    index.js integrated

--- fitmyroom/javascripts/index.js
+++ fitmyroom/javascripts/index.js
@@ -7,6 +7,132 @@ $(document).ready(function() {
 	var nextSlideIndex;
 	var autorun;
 	var sliderDuration = 300;
+	/////////////////////////////////////////////////////////////여기부터
+	var stickyHeaderTop = $(".header").offset().top;
+	var stickyHeader = function() {
+		var scrollTop = $(window).scrollTop();
+		if(scrollTop > stickyHeaderTop){
+			$(".header").addClass('sticky');
+			$(".header").css('border-bottom', '1.5px solid #5c4089');
+			$(".menu").css('visibility', 'visible');
+			$(".menu-deco").css('visibility', 'visible');
+		} else {
+			$(".header").removeClass('sticky');
+			$(".header").css('border-bottom', 'none');
+			$(".menu").css('visibility', 'hidden');
+			$(".menu-deco").css('visibility', 'hidden');
+		}
+	}
+	function lnbShow(){
+		$(".nav-slider").addClass('active');
+		setTimeout(function(){
+			$(".nav-slider.active").css({
+				'height':'2.4rem',
+				'box-shadow':'0px 2px 3px 0px rgba(55,29,97,1)'
+				});
+			$(".nav-slider-arrow").addClass('animate');
+			}, 300);
+		$(".lnb").addClass('active');
+		$(".nav-slider-arrow").addClass('active');
+	}
+	function lnbClose(){
+		$(".lnb").removeClass('active');
+		$(".nav-slider-arrow").removeClass('animate').removeClass('active');
+		$(".nav-slider.active").css({
+			'height':'0.4rem',
+			'box-shadow':'none'
+		});
+		setTimeout(function(){
+			$(".nav-slider").removeClass('active');
+		}, 300);
+	}
+	$("#first").click(function(e){
+		e.stopPropagation;
+		if($(".lnb").hasClass('active')){
+			lnbClose();
+			setTimeout(function(){
+				$(".nav-slider").css('margin-left', '7%');
+				lnbShow();
+			}, 300);
+			$("#first-page").siblings().removeClass('active');
+			$("#first-page").addClass('active');
+		} else {
+			$(".nav-slider").css('margin-left', '7%');
+			lnbShow();
+			$(".nav-slider-arrow").addClass('active');
+			$("#first-page").addClass('active');
+		}
+	});
+	$("#second").click(function(e){
+		e.stopPropagation;
+		if($(".lnb").hasClass('active')){
+			lnbClose();
+			setTimeout(function(){
+				$(".nav-slider").css('margin-left', '31%');
+				lnbShow();
+			}, 300);
+			$("#second-page").siblings().removeClass('active');
+			$("#second-page").addClass('active');
+		} else {
+			$(".nav-slider").css('margin-left', '31%');
+			lnbShow();
+			$(".nav-slider-arrow").addClass('active');
+			$("#second-page").addClass('active');
+		}
+	});
+	$("#third").click(function(e){
+		e.stopPropagation;
+		if($(".lnb").hasClass('active')){
+			lnbClose();
+			setTimeout(function(){
+				$(".nav-slider").css('margin-left', '53%');
+				lnbShow();
+			}, 300);
+			$("#third-page").siblings().removeClass('active');
+			$("#third-page").addClass('active');
+		} else {
+			$(".nav-slider").css('margin-left', '53%');
+			lnbShow();
+			$(".nav-slider-arrow").addClass('active');
+			$("#third-page").addClass('active');
+		}
+	});
+	$("#fourth").click(function(e){
+		e.stopPropagation;
+		if($(".lnb").hasClass('active')){
+			lnbClose();
+			setTimeout(function(){
+				$(".nav-slider").css('margin-left', '74%');
+				$(".nav-slider").addClass('active');
+			}, 300);
+			$(".lnb li").removeClass('active');
+		} else {
+			$(".nav-slider").css('margin-left', '74%');
+			$(".nav-slider").addClass('active');
+			$(".lnb li").removeClass('active');
+		}
+	});
+	$(document).mouseup(function (e) {
+		var container = $(".nav");
+		if (!container.is(e.target) && container.has(e.target).length === 0){
+			lnbClose()
+			$(".lnb").removeClass('active');
+			$(".lnb li").removeClass('active');
+		}	
+	});
+	$(".search-icon").click(function(e){
+		e.stopPropagation;
+		$(".search-area").toggleClass('active');
+		$(".close-search").click(function(e){
+			e.stopPropagation;
+			$(".search-area").removeClass('active');
+		})
+	});
+	stickyHeader();
+	$(window).scroll(function() {
+		stickyHeader();
+	});
+	///////////////////////////////////////////////////////////////////////////여기까지
 	function parallaxOne(){
 		var backgroundHeight = $("#deco1").height();
 		var windowScroll = $(window).scrollTop();
@@ -80,11 +206,19 @@ $(document).ready(function() {
             }, 4000);
         }
 	}
-	autorunSlider();
-		
+	autorunSlider();	
 	parallaxOne();
 	parallaxTwo();
 	parallaxThree();
+	//////////////////////////////////////////////////////////여기부터
+	$(".menu").click(function(e){
+		e.stopPropagation;
+		$(window).scrollTop(0);
+		parallaxOne();
+		parallaxTwo();
+		parallaxThree();
+	});
+	//////////////////////////////////////////////////////////여기까지
 	$(window).scroll(function() {		
 		parallaxOne();
 		parallaxTwo();
