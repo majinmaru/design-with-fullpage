6563b125f076dd70a7913cbc8746153f95beeff4
majinmaru <gotch11@naver.com>
2018-12-19 00:44:54 +0900

    Update section2.js

--- jeager-lecoultre/javascripts/section2.js
+++ jeager-lecoultre/javascripts/section2.js
@@ -2,14 +2,14 @@ $(document).ready(function() {
     $("#section2 .product-info").hide();
     function setBg() {
         $windowSize = $(window).width();
-        if(319 < $windowSize < 769){
+        if($windowSize <= 768){
              $("#section2").css({
                 'background': 'url(images/section2/section2-bg.png), radial-gradient(circle, rgba(4,20,25,1) 0%, rgba(4,20,25,1) 55%, rgba(4,12,12,1) 100%)',
                 'background-size': '55%, 100%',
                 'background-repeat': 'no-repeat',
                 'background-position': '115% bottom, center'
              });
-        } else {
+        } else if($windowSize > 768) {
             $("#section2").css({
                 'background': 'url(images/section2/section2-bg.png), radial-gradient(circle, rgba(4,20,25,1) 0%, rgba(4,20,25,1) 55%, rgba(4,12,12,1) 100%)',
                 'background-size': '25%, 100%',
