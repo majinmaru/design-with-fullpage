83cef894df0fe250f4023210f3c968b5c4843121
majinmaru <gotch11@naver.com>
2019-01-02 02:46:51 +0900

    190102
    basic settings

--- javascripts/introduce.js
+++ javascripts/introduce.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
-	
+	var accentFront = $("<span class='accent front'></span>");
+	var accentBack = $("<span class='accent back'></span>");
+	$(".design").first().addClass('active');
+	$(".design").first().prepend(accentFront);
+	$(".design").first().append(accentBack);
+	$(".ability").hide();
+	$(".ability").first().show();
+
+	$(".design").click(function(e){
+		e.stopPropagation();
+		var $getIndex = $(this).index();
+		$(this).addClass('active');
+		$(this).siblings().removeClass('active');
+		$(this).removeClass(accentFront, accentBack);
+		$(this).prepend(accentFront);
+		$(this).append(accentBack);
+		$(".ability").hide();
+		$(".ability").eq($getIndex).show();
+	});
+	$(window).scroll(function(){
+		var scroll = $(window).scrollTop();
+		var scrollUp = scroll*1.4;
+		$(".section-two").css({
+			transform: "translateY(-"+scrollUp+"px)"
+		});
+
+	});
 });
